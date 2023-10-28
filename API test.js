93df66a76471c5aedd5755ec5c71f209b11d0d53
michaelr16 <michaelr16180@gmail.com>
2023-10-28 17:29:26 -0400

    Added API test.js

---
+++ API test.js
@@ -0,0 +1,10 @@
+getInfo();
+
+async function getInfo(){
+    var response = await fetch("https://na.business-api.amazon.com/products/2020-08-26/products?productRegion=US&locale=en", {headers: {
+            "x-amz-user-email": "akhilvkumar@gmail.com"
+        }
+    });
+    var inJson = await response.json();
+    console.log(inJson.errors[0]);
+}
