937be2110de5e230e10e140be5493b725031b7c8
michaelr16 <michaelr16180@gmail.com>
2023-10-28 17:42:38 -0400

    test commit

--- API test.js
+++ API test.js
@@ -2,7 +2,7 @@ getInfo();
 
 async function getInfo(){
     var response = await fetch("https://na.business-api.amazon.com/products/2020-08-26/products?productRegion=US&locale=en", {headers: {
-            "x-amz-user-email": "akhilvkumar@gmail.com"
+            "x-amz-user-email": ""
         }
     });
     var inJson = await response.json();
