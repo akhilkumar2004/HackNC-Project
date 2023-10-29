d828cf1465affb7b902759fc5f236e220cf91662
michaelr16 <michaelr16180@gmail.com>
2023-10-28 20:56:41 -0400

    linked HTML and JS

--- weather getter.js
+++ weather getter.js
@@ -1,20 +1,26 @@
-async function callAPI(){
+async function callAPI(useddropdown){
     const CITY_INPUT_ID = "search";
     const DROPDOWN_ID = "halloweencostumes";
-    const TEMP_OUTPUT_ID = "testOut";
+    const TEMP_OUTPUT_ID = "tempOut";
 
-    var cityName = document.getElementById(DROPDOWN_ID).value;
-    if (cityName == ""){
-        cityName = document.getElementById(TEMP_OUTPUT_ID).value;
+    
+    if (useddropdown){
+        var cityName = document.getElementById(DROPDOWN_ID).value;
+    } else {
+        cityName = document.getElementById(CITY_INPUT_ID).value;
     }
+    
+    if (cityName == ""){
+        cityName = "New York";
+    }
+    console.log(cityName);
 
     var response = await fetch("https://api.weatherbit.io/v2.0/current?units=I&city=" + cityName + "&key=d418ebdc744e4aeab55db0226a47c060");
     var inJson = await response.json();
 
     temperature = inJson.data[0].temp;
-    console.log(temperature);
     if (temperature != null){
-        document.getElementById(TEMP_OUTPUT_ID).innerText = "" + inJson.data[0].temp;
+        document.getElementById(TEMP_OUTPUT_ID).innerText = cityName + ": " + inJson.data[0].temp + "°F";
     } else {
         document.getElementById(TEMP_OUTPUT_ID).innerText = "No temperature!";
     }
